c106d65543b0fb1ef0861e41b7e70b0dfaab1c9f
bruce <15zlli2@stu.edu.cn>
2018-08-16 09:38:24 +0800

    cmake

---
+++ cmake/cmake-demo/Demo2/CMakeFiles/Demo2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo2.dir/main.cc.o"
+  "CMakeFiles/Demo2.dir/MathFunctions.cc.o"
+  "Demo2.pdb"
+  "Demo2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Demo2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
