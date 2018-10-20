58a5fa6ea2b9dfe0fbd20041b190421c8f7c807b
Bruce <15zlli@stu.edu.cn>
2018-10-20 17:54:33 +0800

    机器人运动

---
+++ c_language/offer/movingCount/cmake-build-debug/CMakeFiles/movingCount.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/movingCount.dir/main.cpp.o"
+  "movingCount.pdb"
+  "movingCount"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/movingCount.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
